592d5f0544b52845d0f9d48772354da983216723
sairamtorlikonda <42658966+sairamtorlikonda@users.noreply.github.com>
2018-09-14 20:55:07 -0500

    Add files via upload

---
+++ ICP4/source/LoginActivity.java
@@ -0,0 +1,72 @@
+package tutorial.cs5551.com.translateapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+//AppCompatActivity:
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void checkCredentials(View v)
+    {
+        EditText usernameCtrl = (EditText)findViewById(R.id.txt_uname);
+        EditText passwordCtrl = (EditText) findViewById(R.id.txt_Pwd);
+        TextView errorText = (TextView)findViewById(R.id.lbl_Error);
+        String userName = usernameCtrl.getText().toString();
+        String password = passwordCtrl.getText().toString();
+
+        boolean validationFlag = false;
+        if(!userName.isEmpty() && !password.isEmpty()) {
+            if(userName.equals("sairam") && password.equals("sairam")) {
+                validationFlag = true;
+
+            }
+        }
+        if(!validationFlag)
+        {
+            errorText.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            Intent redirect = new Intent(LoginActivity.this, TranslateActivity.class);
+            startActivity(redirect);
+        }
+
+    }
+}
